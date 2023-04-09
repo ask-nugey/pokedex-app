dcf4b7a7b1b7141b5ca1513e5458db2bdf86b1b6
yuki-iwase <mischief.moi@gmail.com>
2023-04-10 00:40:41 +0900

    🚀 Added feature to fetch all data from pokeapi.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+    };
+  }, [])
+
   return (
-    <></>
+    <>
+      <div></div>
+    </>
   );
 }
 
