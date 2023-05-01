f64959463c0da2a33ee1cde5aa74dc1e53d1deff
yuki-iwase <mischief.moi@gmail.com>
2023-05-02 03:36:24 +0900

    ✨ Created card component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon, getPokemon } from './utils/pokemon';
+import Card from './components/Card';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true)
-  const [pokemonData, setPokemonData] = useState()
+  const [pokemonData, setPokemonData] = useState(null)
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
       // 各ポケモンの詳細データを取得
-      loadPokemon(res.results)
-      setLoading(false)
+      loadPokemon(res.results);
+      setLoading(false);
     }
     fetchPokemonData()
   }, [])
@@ -28,15 +29,17 @@ function App() {
     setPokemonData(_polemonData)
   }
 
-  console.log(pokemonData)
-
   return (
     <>
       <div>
         {loading ? (
           <h1>ロード中</h1>
         ) : (
-          <h1>ポケモンデータを取得</h1>
+          <div className="conainer">
+            {pokemonData?.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
         )}
       </div>
     </>
