47ec5fb42971002ae87ce0c312261b0dbcbab77d
yuki-iwase <mischief.moi@gmail.com>
2023-05-02 19:51:50 +0900

    ✨ Created pager component.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true)
   const [pokemonData, setPokemonData] = useState(null)
+  const [nextURL, setNextURL] = useState("")
+  const [prevURL, setPrevURL] = useState("")
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -15,6 +17,7 @@ function App() {
       let res = await getAllPokemon(initialURL);
       // 各ポケモンの詳細データを取得
       await loadPokemon(res.results);
+      setNextURL(res.next);
       setLoading(false);
     }
     fetchPokemonData()
@@ -30,6 +33,28 @@ function App() {
     setPokemonData(_polemonData)
   }
 
+  const handlePrevPage = async () => {
+    if (!prevURL) return
+
+    setLoading(true)
+    let data = await getAllPokemon(prevURL)
+    await loadPokemon(data.results);
+    setNextURL(data.next)
+    setPrevURL(data.previous)
+    setLoading(false);
+  }
+
+  const handleNextPage = async () => {
+    if (!nextURL) return
+
+    setLoading(true)
+    let data = await getAllPokemon(nextURL)
+    await loadPokemon(data.results);
+    setNextURL(data.next)
+    setPrevURL(data.previous)
+    setLoading(false)
+  }
+
   return (
     <>
       <Header />
@@ -37,11 +62,17 @@ function App() {
         {loading ? (
           <h1>ロード中</h1>
         ) : (
-          <div className="conainer">
-            {pokemonData.map((pokemon, i) => {
-              return <Card key={i} pokemon={pokemon} />;
-            })}
-          </div>
+          <>
+            <div className="conainer">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+            <div className="pager">
+              <button onClick={handlePrevPage}>前へ</button>
+              <button onClick={handleNextPage}>次へ</button>
+            </div>
+          </>
         )}
       </div>
     </>
